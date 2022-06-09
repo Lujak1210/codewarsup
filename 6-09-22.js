5d2867261ce7a9d44fd356f4019fdf0e43149a8b
Luis Garcia <lujak1234@gmail.com>
2022-06-09 14:03:32 -0700

    daily push

---
+++ 6-09-22.js
@@ -0,0 +1,7 @@
+// Bin to Decimal
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+    return parseInt(bin,2);  //parseInt() braks down string in bin , and converts to the amount of decimals given, in this case 2 as it asks for a binary number.
+}
