dedf8e22227e5f7a753e96b94d6e03f71531bf38
Luis Garcia <lujak1234@gmail.com>
2022-06-28 18:17:48 -0700

    daily push

---
+++ 6-28-22.js
@@ -0,0 +1,9 @@
+// function 2: squaring argument.
+
+// DESCRIPTION:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+function square(n1){
+    return n1 * n1
+}
