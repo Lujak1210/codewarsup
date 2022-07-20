a8aff051ff285bc6df0aab6e3200e47a96b85451
Luis Garcia <lujak1234@gmail.com>
2022-07-19 20:07:42 -0700

    daily push

---
+++ 7-19-22.js
@@ -0,0 +1,28 @@
+//  Keep HYdrated
+
+//DESCRIPTION:
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+
+// first blind try. (WRONG)
+function litres(time) {
+    return Math.min(time * .33) ;
+  }
+
+
+  // the way it should be.
+  function litres(time) {
+    return Math.floor(time * 0.5);
+  }
