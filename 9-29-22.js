5eec62ee7aacdd396833894223442109c271d7af
Luis Garcia <lujak1234@gmail.com>
2022-09-29 18:40:45 -0700

    daily up

---
+++ 9-29-22.js
@@ -0,0 +1,13 @@
+// reversed strings
+
+
+// DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+    return str.split('').reverse().join(''); // split that thang, reverse them and join back.  
+  }
+
