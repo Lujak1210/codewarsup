7b0041866e39405ccc44c603c0053b72655e06fc
Luis Garcia <lujak1234@gmail.com>
2022-10-02 19:08:18 -0700

    daily up

---
+++ 10-2-22.js
@@ -0,0 +1,13 @@
+// remove exclamation marks
+
+// DESCRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+// this ol chestnut
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/g, '');
+  }
+
+  // look up replacwe dummy 
