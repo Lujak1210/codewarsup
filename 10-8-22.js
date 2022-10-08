5e2e655deea2446364c7c77d929465b462507c31
Luis Garcia <lujak1234@gmail.com>
2022-10-08 11:29:15 -0700

    daily rep

---
+++ 10-8-22.js
@@ -0,0 +1,10 @@
+// Kata Example twist
+
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+
+var websites = [];
+while (websites.length < 1000) websites.push("codewars")
+
+// while loop with a .length, if length is less that 1000 push a websites into an array  
