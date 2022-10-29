bd49674629ac9f47858b4856ff26523895332000
Luis Garcia <lujak1234@gmail.com>
2022-10-28 19:57:19 -0700

    Daily rep

---
+++ 10-28-22.js
@@ -0,0 +1,8 @@
+// Remove String Spaces
+
+// Description:
+
+// Simple, remove the spaces form the string, then return the resultant string.
+
+function noSpace(x){return x.split(' ').join('')}
+//function noSpace will take in a string(x) and return a splited and joined string, spaces removed.
